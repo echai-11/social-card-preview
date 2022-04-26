a52197dc93b295de4ca4605c02586a8ab9c019bb
Lizzie Chai <lizzie.chai@gmail.com>
2022-04-26 12:48:13 -0400

    additional tags

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -20,11 +20,11 @@ export const TWITTER_TITLE = "twitter:title";
 export const TWITTER_DESCRIPTION = "twitter:description";
 export const TWITTER_IMAGE = "twitter:image";
 export const TWITTER_IMAGE_ALT = "twitter:image:alt";
-/*Twitter Player Card Required Values*/
+//Twitter Player Card Required Values
 export const TWITTER_PLAYER = "twitter:player"; //HTTPS URL to iFrame player.
 export const TWITTER_PLAYER_WIDTH = "twitter:player:width";
 export const TWITTER_PLAYER_HEIGHT = "twitter:player:height";
-//Twiter App Card Values
+//Twitter App Card Values
 export const TWITTER_APP_ID_IPHONE = "twitter:app:id:iphone"; //required
 export const TWITTER_APP_ID_IPAD = "twitter:app:id:ipad"; //required
 export const TWITTER_APP_ID_GOOGLEPLAY = "twitter:app:id:googleplay"; //required
@@ -32,3 +32,12 @@ export const TWITTER_APP_URL_IPHONE = "twitter:app:url:iphone";
 export const TWITTER_APP_URL_IPAD = "twitter:app:url:ipad";
 export const TWITTER_APP_URL_GOOGLEPLAY = "twitter:app:url:googleplay";
 export const TWITTER_APP_COUNTRY = "twitter:app:country";
+
+export const UPDATED_TIME = "og:updated_time";
+export const FB_APP_ID="fb:app_id"
+
+//Article Tags
+export const ARTICLE_PUBLISHED_TIME="article:published_time"
+export const ARTICLE_MODIFIED_TIME = "article:modified_time";
+export const ARTICLE_TAG="article:tag";
+export const ARTICLE_SECTION="article:section"
